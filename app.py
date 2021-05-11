cb4b9d66f26e4a83837f2d828700f6ce80b44aa5
Aaron Gruberg <aarongruberg@Aarons-MBP.attlocal.net>
2021-05-11 15:15:39 -0700

    testing sleep of 40 seconds

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 
 # Example with index.html
 from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    time.sleep(40)
     return render_template('index.html')
